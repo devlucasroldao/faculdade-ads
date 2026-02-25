526ab94f1a6e44e853f3277637e88ebf0afcc964
Lucas Roldão Cardoso <lucasroldao2802@gmail.com>
2026-02-25 20:50:52 -0300

    Atividade nova

--- back-end/MeuPrimeiroAppConsole/Program.cs
+++ back-end/MeuPrimeiroAppConsole/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Olá, Universo! Eu me chamo Lucas e sou um desenvolvedor .NET. 🚀");
+
+int numero = 10;
+double pi = 3.1415;
+char letra = "U+0041";
+char letraA = "A";
+bool isTrue = false;
+string mensagem = "Olá, Mundo!";
+
+Console.WriteLine($"{letra}  {letraA}");
+
+Console.WriteLine("Bem-vindo ao C#");
+Console.WriteLine("Informe seu nome: ");
+nome = Console.ReadLine();
+Console.WriteLine("Informe sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Seu nome é {nome} e tenho {idade} anos de idade!");
